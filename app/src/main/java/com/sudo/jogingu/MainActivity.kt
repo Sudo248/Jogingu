58e3acc5dbafe7d7a60c01fd0ce06838107e8fdb
Dang Dung <dangdung239>
2021-10-21 23:43:16 +0700

    test

--- app/src/main/java/com/sudo/jogingu/MainActivity.kt
+++ app/src/main/java/com/sudo/jogingu/MainActivity.kt
@@ -2,11 +2,21 @@ package com.sudo.jogingu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
     }
 }

c60a6f0e63a42dd34f6a94ca0015c7c661b380fb
Sudo248 <hongduong080501@gmail.com>
2021-10-22 00:10:30 +0700

    delete

--- app/src/main/java/com/sudo/jogingu/MainActivity.kt
+++ app/src/main/java/com/sudo/jogingu/MainActivity.kt
@@ -8,13 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        sayHi()
-        sad()
+
     }
-    fun sayHi(){
-        print("hello")
-    }
-    fun sad(){
-        print("fdsfds")
-    }
+
 }

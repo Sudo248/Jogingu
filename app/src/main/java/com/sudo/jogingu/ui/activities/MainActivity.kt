f5d4ca5720ec2f7aec553de5e345384d667f3cb7
Sudo248 <hongduong080501@gmail.com>
2021-10-23 19:05:38 +0700

    add timber for log

--- app/src/main/java/com/sudo/jogingu/ui/activities/MainActivity.kt
+++ app/src/main/java/com/sudo/jogingu/ui/activities/MainActivity.kt
@@ -3,12 +3,14 @@ package com.sudo.jogingu.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sudo.jogingu.R
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
 }
